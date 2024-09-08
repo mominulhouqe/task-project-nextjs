3c186a026e609f5e8b47b1e1d2dc02dd9d504523
Mominul <ulhouqemomin39@gmail.com>
2024-09-09 01:02:40 +0600

    added testimonial

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,19 @@
 import EnginnerTeam from "@/components/ui/EnginnerTeam";
 import ProductTeam from "@/components/ui/ProductTeam";
 import Quebly from "@/components/ui/Quebly";
+import Testmonial from "@/components/ui/Testmonial";
 
 const HomePage = async () => {
   return (
-    <div className="md:w-[960px] mx-auto">
+   <div>
+     <div className="md:w-[960px] mx-auto">
       <Quebly />
       <ProductTeam />
       <EnginnerTeam />
     </div>
+      <Testmonial />
+    
+   </div>
   );
 };
 
