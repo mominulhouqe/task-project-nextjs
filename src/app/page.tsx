d9eebeffa24f6730d243caf23a31a8d38cb95b5b
Mominul <ulhouqemomin39@gmail.com>
2024-09-08 23:51:02 +0600

    Qubly

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+import Quebly from "@/components/ui/Quebly";
+
 const HomePage = async () => {
   return (
     <>
-      <h1 className="text-center text-4xl my-5">Latest Blogs</h1>
+      <Quebly />
     </>
   );
 };
